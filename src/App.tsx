04012825876f0e2e8ee16b7879b8864c4ebca356
Wony-Lee <a1_11_11@naver.com>
2022-01-05 23:03:48 +0900

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,70 @@
 import React, { useEffect, useState } from "react";
-import Carousel from "./components/Carousel";
+import axios from "axios";
+import Pagination from "./components/Pagination";
+
+interface Airline {
+    id: number;
+    name: string;
+    country: string;
+    logo: string;
+    slogan: string;
+    head_quaters: string;
+    website: string;
+    established: string;
+}
+
+interface Passenger {
+    _id: string;
+    name: string;
+    trips: number;
+    airline: Airline;
+    __v: number;
+}
+
+interface Response {
+    totalPassengers: number;
+    totalPages: number;
+    data: Array<Passenger>;
+}
 
 function App() {
-    return <Carousel />;
+    const [page, setPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [items, setItems] = useState<Array<Passenger>>([]);
+
+    const handlePageChange = (currentPage: number): void => {
+        setPage(currentPage);
+    };
+
+    useEffect(() => {
+        const fetch = async () => {
+            const params = { page, size: 10 };
+            const {
+                data: { totalPages, data },
+            } = await axios.get<Response>(
+                "https://api.instantwebtools.net/passenger",
+                { params }
+            );
+            setTotalPages(totalPages);
+            setItems(data);
+        };
+        fetch();
+    }, []);
+
+    return (
+        <div className="App">
+            <ul>
+                {items.map((item) => (
+                    <li key={item._id}>{item.name}</li>
+                ))}
+            </ul>
+            <Pagination
+                count={totalPages}
+                page={page}
+                onPageChange={handlePageChange}
+            ></Pagination>
+        </div>
+    );
 }
 
 export default App;
